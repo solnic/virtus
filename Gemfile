eca99458c20ba6a4fc67dc9dbbba97bd7fcba451
Piotr Solnica <piotr.solnica@gmail.com>
2011-05-21 18:52:09 +0200

    Add SimpleCov dev dependency

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source "http://rubygems.org"
 gemspec
 
 group :development do
-  gem "jeweler", "~> 1.5.2"
-  gem "rspec",   "~> 2.4.0"
+  gem "jeweler",   "~> 1.5.2"
+  gem "rspec",     "~> 2.4.0"
+  gem "simplecov", "~> 0.4.2", :platforms => [ :mri_19 ]
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
 require 'pathname'
 require 'rubygems'
 require 'rspec'
-require 'virtus'
 
+if ENV['SPEC_COV'] && RUBY_VERSION >= '1.9.2'
+  require 'simplecov'
+
+  SimpleCov.start do
+    add_filter "/spec/"
+    add_group "lib", "lib"
+  end
+end
+
+require 'virtus'
