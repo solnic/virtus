9653d9b6ead81bd097812852233ed65b6818c38f
Piotr Solnica <piotr.solnica@gmail.com>
2011-06-01 21:11:53 +0200

    Add integration specs for complex attributes dirty tracking

--- spec/integration/attributes/array_spec.rb
+++ spec/integration/attributes/array_spec.rb
@@ -6,4 +6,33 @@ describe Virtus::Attributes::Array do
     let(:attribute_value)       { [ 'red', 'green', 'blue' ] }
     let(:attribute_value_other) { [ 'orange', 'yellow', 'gray' ] }
   end
+
+  describe 'dirty tracking' do
+    let(:model) do
+      Class.new do
+        include Virtus
+        include Virtus::DirtyTracking
+
+        attribute :colors, Array
+      end
+    end
+
+    let(:object) do
+      model.new(:colors => [])
+    end
+
+    context "when value is set implicitly" do
+      before do
+        object.colors << 'gray'
+      end
+
+      it "marks the attribute as dirty" do
+        object.attribute_dirty?(:colors).should be(true)
+      end
+
+      it "sets dirty attributes hash" do
+        object.dirty_attributes.should == { :colors => ['gray'] }
+      end
+    end
+  end
 end
--- spec/integration/attributes/hash_spec.rb
+++ spec/integration/attributes/hash_spec.rb
@@ -6,4 +6,33 @@ describe Virtus::Attributes::Hash do
     let(:attribute_value)       { Hash[:one => 1] }
     let(:attribute_value_other) { Hash[:two => 2] }
   end
+
+  describe 'dirty tracking' do
+    let(:model) do
+      Class.new do
+        include Virtus
+        include Virtus::DirtyTracking
+
+        attribute :settings, Hash
+      end
+    end
+
+    let(:object) do
+      model.new(:settings => {})
+    end
+
+    context "when value is set implicitly" do
+      before do
+        object.settings[:one] = '1'
+      end
+
+      it "marks the attribute as dirty" do
+        object.attribute_dirty?(:settings).should be(true)
+      end
+
+      it "sets dirty attributes hash" do
+        object.dirty_attributes.should == { :settings => { :one => '1' } }
+      end
+    end
+  end
 end
