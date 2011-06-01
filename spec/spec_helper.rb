280d311ee0409f07531d1106be85e4dec60e02e2
Piotr Solnica <piotr.solnica@gmail.com>
2011-06-01 21:30:38 +0200

    Fixed issues with loading shared examples

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,3 +14,7 @@ end
 require 'virtus'
 
 ENV['TZ'] = 'UTC'
+
+SPEC_ROOT = Pathname(__FILE__).dirname.expand_path
+
+Pathname.glob((SPEC_ROOT + '**/shared/**/*.rb').to_s).each { |file| require file }
