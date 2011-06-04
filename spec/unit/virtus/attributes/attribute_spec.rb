05cd281d34bf893ed44d423a9ab5e9aee3cb2c80
Piotr Solnica <piotr.solnica@gmail.com>
2011-06-04 17:18:01 +0200

    Add Attribute#typecast_to_primitive spec

---
+++ spec/unit/virtus/attributes/attribute_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Virtus::Attributes::Attribute do
+  describe '#typecast_to_primitive' do
+    let(:model)     { Class.new { include Virtus } }
+    let(:attribute) { Virtus::Attributes::Attribute.new(:name, model) }
+    let(:value)     { 'value' }
+
+    it "returns original value" do
+      attribute.typecast_to_primitive(value, model).should == value
+    end
+  end
+end
