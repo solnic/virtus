ffb6d110ff460a9568e29bf5cea04744f1d3d2db
Dan Kubb <dan.kubb@gmail.com>
2011-06-06 12:11:11 -0700

    Avoid unnecessary object creation when the Array is constant

--- lib/virtus/attributes/typecast/time.rb
+++ lib/virtus/attributes/typecast/time.rb
@@ -4,6 +4,8 @@ module Virtus
       module Time
         include Numeric
 
+        SEGMENTS = [ :year, :month, :day, :hour, :min, :sec ].freeze
+
         # Extracts the given args from the hash. If a value does not exist, it
         # uses the value of Time.now.
         #
@@ -17,7 +19,7 @@ module Virtus
         def extract_time(value)
           now  = ::Time.now
 
-          [ :year, :month, :day, :hour, :min, :sec ].map do |segment|
+          SEGMENTS.map do |segment|
             typecast_to_numeric(value.fetch(segment, now.send(segment)), :to_i)
           end
         end
