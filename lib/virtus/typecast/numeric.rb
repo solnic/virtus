18eca233111aae64bbefc0867a120dafebc71a89
Piotr Solnica <piotr.solnica@gmail.com>
2011-06-09 11:26:18 +0200

    Remove class << self from Typecast classes

--- lib/virtus/typecast/numeric.rb
+++ lib/virtus/typecast/numeric.rb
@@ -2,69 +2,67 @@ module Virtus
   module Typecast
     # Typecast numeric values. Supports Integer, Float and BigDecimal
     class Numeric
-      class << self
-        # Typecast value to integer
-        #
-        # @param [Object]
-        #
-        # @return [Integer]
-        #
-        # @api public
-        def to_i(value)
-          call(value, :to_i)
+      # Typecast value to integer
+      #
+      # @param [Object]
+      #
+      # @return [Integer]
+      #
+      # @api public
+      def self.to_i(value)
+        call(value, :to_i)
+      end
+
+      # Typecast value to float
+      #
+      # @param [Object]
+      #
+      # @return [Float]
+      #
+      # @api public
+      def self.to_f(value)
+        call(value, :to_f)
+      end
+
+      # Typecast value to decimal
+      #
+      # @param [Object]
+      #
+      # @return [BigDecimal]
+      #
+      # @api public
+      def self.to_d(value)
+        if value.kind_of?(::Integer)
+          value.to_s.to_d
+        else
+          call(value, :to_d)
         end
+      end
 
-        # Typecast value to float
-        #
-        # @param [Object]
-        #
-        # @return [Float]
-        #
-        # @api public
-        def to_f(value)
-          call(value, :to_f)
-        end
+      private
 
-        # Typecast value to decimal
-        #
-        # @param [Object]
-        #
-        # @return [BigDecimal]
-        #
-        # @api public
-        def to_d(value)
-          if value.kind_of?(::Integer)
-            value.to_s.to_d
-          else
-            call(value, :to_d)
-          end
-        end
-
-        private
-
-        # Match numeric string
-        #
-        # @param [#to_str, Numeric] value
-        #   value to typecast
-        # @param [Symbol] method
-        #   method to typecast with
-        #
-        # @return [Numeric]
-        #   number if matched, value if no match
-        #
-        # @api private
-        def call(value, method)
-          if value.respond_to?(:to_str)
-            if value.to_str =~ /\A(-?(?:0|[1-9]\d*)(?:\.\d+)?|(?:\.\d+))\z/
-              $1.send(method)
-            else
-              value
-            end
-          elsif value.respond_to?(method)
-            value.send(method)
+      # Match numeric string
+      #
+      # @param [#to_str, Numeric] value
+      #   value to typecast
+      # @param [Symbol] method
+      #   method to typecast with
+      #
+      # @return [Numeric]
+      #   number if matched, value if no match
+      #
+      # @api private
+      def self.call(value, method)
+        if value.respond_to?(:to_str)
+          if value.to_str =~ /\A(-?(?:0|[1-9]\d*)(?:\.\d+)?|(?:\.\d+))\z/
+            $1.send(method)
           else
             value
           end
+        elsif value.respond_to?(method)
+          value.send(method)
+        else
+          value
         end
       end
     end # Numeric
--- lib/virtus/typecast/time.rb
+++ lib/virtus/typecast/time.rb
@@ -10,133 +10,131 @@ module Virtus
         :to_datetime => ::DateTime
       }.freeze
 
-      class << self
-        # Typecasts an arbitrary value to a Time
-        # Handles both Hashes and Time instances.
-        #
-        # @param [Hash, #to_mash, #to_s] value
-        #   value to be typecast
-        #
-        # @return [Time]
-        #   Time constructed from value
-        #
-        # @api public
-        def to_time(value)
-          call(value, :to_time)
-        end
+      # Typecasts an arbitrary value to a Time
+      # Handles both Hashes and Time instances.
+      #
+      # @param [Hash, #to_mash, #to_s] value
+      #   value to be typecast
+      #
+      # @return [Time]
+      #   Time constructed from value
+      #
+      # @api public
+      def self.to_time(value)
+        call(value, :to_time)
+      end
 
-        # Typecasts an arbitrary value to a Date
-        # Handles both Hashes and Date instances.
-        #
-        # @param [Hash, #to_mash, #to_s] value
-        #   value to be typecast
-        #
-        # @return [Date]
-        #   Date constructed from value
-        #
-        # @api public
-        def to_date(value)
-          call(value, :to_date)
-        end
+      # Typecasts an arbitrary value to a Date
+      # Handles both Hashes and Date instances.
+      #
+      # @param [Hash, #to_mash, #to_s] value
+      #   value to be typecast
+      #
+      # @return [Date]
+      #   Date constructed from value
+      #
+      # @api public
+      def self.to_date(value)
+        call(value, :to_date)
+      end
 
-        # Typecasts an arbitrary value to a DateTime.
-        # Handles both Hashes and DateTime instances.
-        #
-        # @param [Hash, #to_mash, #to_s] value
-        #   value to be typecast
-        #
-        # @return [DateTime]
-        #   DateTime constructed from value
-        #
-        # @api public
-        def to_datetime(value)
-          call(value, :to_datetime)
-        end
+      # Typecasts an arbitrary value to a DateTime.
+      # Handles both Hashes and DateTime instances.
+      #
+      # @param [Hash, #to_mash, #to_s] value
+      #   value to be typecast
+      #
+      # @return [DateTime]
+      #   DateTime constructed from value
+      #
+      # @api public
+      def self.to_datetime(value)
+        call(value, :to_datetime)
+      end
 
-        private
+      private
 
-        # @api private
-        def call(value, method)
-          return value.send(method) if value.respond_to?(method)
+      # @api private
+      def self.call(value, method)
+        return value.send(method) if value.respond_to?(method)
 
-          begin
-            if value.is_a?(::Hash)
-              from_hash(value, method)
-            else
-              from_string(value.to_s, method)
-            end
-          rescue ArgumentError
-            return value
+        begin
+          if value.is_a?(::Hash)
+            from_hash(value, method)
+          else
+            from_string(value.to_s, method)
           end
+        rescue ArgumentError
+          return value
         end
+      end
 
-        # @api private
-        def from_string(value, method)
-          METHOD_TO_CLASS[method].parse(value.to_s)
-        end
+      # @api private
+      def self.from_string(value, method)
+        METHOD_TO_CLASS[method].parse(value.to_s)
+      end
 
-        # @api private
-        def from_hash(value, method)
-          send("hash_#{method}", value)
-        end
+      # @api private
+      def self.from_hash(value, method)
+        send("hash_#{method}", value)
+      end
 
-        # Creates a Time instance from a Hash with keys :year, :month, :day,
-        # :hour, :min, :sec
-        #
-        # @param [Hash, #to_mash] value
-        #   value to be typecast
-        #
-        # @return [Time]
-        #   Time constructed from hash
-        #
-        # @api private
-        def hash_to_time(value)
-          ::Time.local(*extract(value))
-        end
+      # Creates a Time instance from a Hash with keys :year, :month, :day,
+      # :hour, :min, :sec
+      #
+      # @param [Hash, #to_mash] value
+      #   value to be typecast
+      #
+      # @return [Time]
+      #   Time constructed from hash
+      #
+      # @api private
+      def self.hash_to_time(value)
+        ::Time.local(*extract(value))
+      end
 
-        # Creates a Date instance from a Hash with keys :year, :month, :day
-        #
-        # @param [Hash, #to_mash] value
-        #   value to be typecast
-        #
-        # @return [Date]
-        #   Date constructed from hash
-        #
-        # @api private
-        def hash_to_date(value)
-          ::Date.new(*extract(value).first(3))
-        end
+      # Creates a Date instance from a Hash with keys :year, :month, :day
+      #
+      # @param [Hash, #to_mash] value
+      #   value to be typecast
+      #
+      # @return [Date]
+      #   Date constructed from hash
+      #
+      # @api private
+      def self.hash_to_date(value)
+        ::Date.new(*extract(value).first(3))
+      end
 
-        # Creates a DateTime instance from a Hash with keys :year, :month, :day,
-        # :hour, :min, :sec
-        #
-        # @param [Hash, #to_mash] value
-        #   value to be typecast
-        #
-        # @return [DateTime]
-        #   DateTime constructed from hash
-        #
-        # @api private
-        def hash_to_datetime(value)
-          ::DateTime.new(*extract(value))
-        end
+      # Creates a DateTime instance from a Hash with keys :year, :month, :day,
+      # :hour, :min, :sec
+      #
+      # @param [Hash, #to_mash] value
+      #   value to be typecast
+      #
+      # @return [DateTime]
+      #   DateTime constructed from hash
+      #
+      # @api private
+      def self.hash_to_datetime(value)
+        ::DateTime.new(*extract(value))
+      end
 
-        # Extracts the given args from the hash. If a value does not exist, it
-        # uses the value of Time.now.
-        #
-        # @param [Hash, #to_mash] value
-        #   value to extract time args from
-        #
-        # @return [Array]
-        #   Extracted values
-        #
-        # @api public
-        def extract(value)
-          now = ::Time.now
+      # Extracts the given args from the hash. If a value does not exist, it
+      # uses the value of Time.now.
+      #
+      # @param [Hash, #to_mash] value
+      #   value to extract time args from
+      #
+      # @return [Array]
+      #   Extracted values
+      #
+      # @api public
+      def self.extract(value)
+        now = ::Time.now
 
-          SEGMENTS.map do |segment|
-            Numeric.to_i(value.fetch(segment, now.send(segment)))
-          end
+        SEGMENTS.map do |segment|
+          Numeric.to_i(value.fetch(segment, now.send(segment)))
         end
       end
     end # Time
