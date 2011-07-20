f0f4997a04b86e8a2583b8e5fe0db32a491d46aa
Piotr Solnica <piotr.solnica@gmail.com>
2011-07-20 12:14:32 +0200

    Add Typecast::Hash.to_array

--- spec/unit/virtus/attribute/array_spec.rb
+++ spec/unit/virtus/attribute/array_spec.rb
@@ -6,4 +6,17 @@ describe Virtus::Attribute::Array do
     let(:attribute_value)       { [ 'red', 'green', 'blue' ] }
     let(:attribute_value_other) { [ 'orange', 'yellow', 'gray' ] }
   end
+
+  describe '#typecast' do
+    let(:attribute) { described_class.new(:colors) }
+
+    subject { attribute.typecast(value) }
+
+    context 'with a Hash' do
+      let(:value) { { :foo => 'bar' } }
+
+      it { should == [ [:foo, 'bar'] ] }
+    end
+  end
+
 end
