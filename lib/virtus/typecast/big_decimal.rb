a3f509bb39831bd45f32dbe1edae695b7a5b42b3
Piotr Solnica <piotr.solnica@gmail.com>
2011-07-20 12:23:10 +0200

    Add missing docs for Typecast::BigDecimal

--- lib/virtus/typecast/big_decimal.rb
+++ lib/virtus/typecast/big_decimal.rb
@@ -5,11 +5,29 @@ module Virtus
     #
     class BigDecimal < Object
 
+      # Creates a Fixnum instance from a BigDecimal
+      #
+      # @example
+      #   Virtus::Typecast::BigDecimal.to_i(BigDecimal('1.0')) # => 1
+      #
+      # @param [BigDecimal] value
+      #
+      # @return [Fixnum]
+      #
       # @api public
       def self.to_i(value)
         value.to_i
       end
 
+      # Creates a Float instance from a BigDecimal
+      #
+      # @example
+      #   Virtus::Typecast::BigDecimal.to_f(BigDecimal('1.0')) # => 1.0
+      #
+      # @param [BigDecimal] value
+      #
+      # @return [Fixnum]
+      #
       # @api public
       def self.to_f(value)
         value.to_f
