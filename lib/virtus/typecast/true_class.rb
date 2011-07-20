36ce56246974310eb8b5ccf17828a47d3b8c0e2f
Dan Kubb <dan.kubb@gmail.com>
2011-07-20 10:58:09 -0700

    Added missing to_string method to Typecast::TrueClass and Typecast::FalseClass
    * The plan is to extract this into a shared module, but for now specify this
      explicitly.

--- lib/virtus/typecast/true_class.rb
+++ lib/virtus/typecast/true_class.rb
@@ -5,6 +5,20 @@ module Virtus
     #
     class TrueClass < Object
 
+      # Typecast given value to String
+      #
+      # @example
+      #   Virtus::Typecast::TrueClass.to_string(true) # => "true"
+      #
+      # @param [TrueClass] value
+      #
+      # @return [String]
+      #
+      # @api public
+      def self.to_string(value)
+        value.to_s
+      end
+
     end # class TrueClass
   end # module Typecast
 end # module Virtus
