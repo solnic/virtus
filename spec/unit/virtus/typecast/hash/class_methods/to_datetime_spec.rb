a2d77ca13ddd5faa8d1bfe04060b7483e1c3ec4b
Dan Kubb <dan.kubb@gmail.com>
2011-07-20 14:40:33 -0700

    Added specs for Typecast::Hash

---
+++ spec/unit/virtus/typecast/hash/class_methods/to_datetime_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Virtus::Typecast::Hash, '.to_datetime' do
+  subject { object.to_datetime(hash) }
+
+  let(:object) { described_class }
+
+  context 'when time segments are missing' do
+    let(:time_now) { Time.local(2011, 1, 1) }
+    let(:hash)     { {}                     }
+
+    before do
+      now = time_now
+      Time.stub!(:now).and_return(now)  # freeze time
+    end
+
+    it { should be_instance_of(DateTime) }
+
+    it 'uses the Time now to populate the segments' do
+      should eql(DateTime.new(2011, 1, 1))
+    end
+  end
+
+  context 'when time segments are populated' do
+    let(:hash) { { :year => 2011, :month => 1, :day => 1, :hour => 1, :min => 1, :sec => 1 } }
+
+    it { should be_instance_of(DateTime) }
+
+    it { should eql(DateTime.new(2011, 1, 1, 1, 1, 1)) }
+  end
+end
---
+++ spec/unit/virtus/typecast/hash/class_methods/to_time_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Virtus::Typecast::Hash, '.to_time' do
+  subject { object.to_time(hash) }
+
+  let(:object) { described_class }
+
+  context 'when time segments are missing' do
+    let(:time_now) { Time.local(2011, 1, 1) }
+    let(:hash)     { {}                     }
+
+    before do
+      now = time_now
+      Time.stub!(:now).and_return(now)  # freeze time
+    end
+
+    it { should be_instance_of(Time) }
+
+    it 'uses the Time now to populate the segments' do
+      should eql(time_now)
+    end
+  end
+
+  context 'when time segments are populated' do
+    let(:hash) { { :year => 2011, :month => 1, :day => 1, :hour => 1, :min => 1, :sec => 1 } }
+
+    it { should be_instance_of(Time) }
+
+    it { should eql(Time.local(2011, 1, 1, 1, 1, 1)) }
+  end
+end
