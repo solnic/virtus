c21b284d51a68fa106bb56f13f360ba40893b5d7
Piotr Solnica <piotr.solnica@gmail.com>
2011-07-22 13:52:45 +0200

    Add Typecast::String.to_datetime specs

---
+++ spec/unit/virtus/typecast/string/class_methods/to_datetime_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe Virtus::Typecast::String, '.to_datetime' do
+  subject { described_class.to_datetime(string) }
+
+  shared_examples_for 'a correct datetime object' do
+    it { should be_instance_of(DateTime) }
+
+    its(:year)  { should == year  }
+    its(:month) { should == month }
+    its(:day)   { should == day   }
+    its(:hour)  { should == hour  }
+    its(:min)   { should == min   }
+    its(:sec)   { should == sec   }
+  end
+
+  context 'with a valid date string' do
+    let(:year)  { 2011 }
+    let(:month) { 7    }
+    let(:day)   { 22   }
+
+    context 'not including time part' do
+      let(:string) { "July, #{day}th, #{year}" }
+
+      let(:hour) { 0 }
+      let(:min)  { 0 }
+      let(:sec)  { 0 }
+
+      it_behaves_like 'a correct datetime object'
+    end
+
+    context 'including time part' do
+      let(:string) { "July, #{day}, #{year}, #{hour}:#{min}:#{sec}" }
+
+      let(:hour) { 13 }
+      let(:min)  { 44 }
+      let(:sec)  { 50 }
+
+      it_behaves_like 'a correct datetime object'
+    end
+  end
+
+  context 'with an invalid date time string' do
+    let(:string) { 'non-datetime' }
+
+    it { should == string }
+  end
+end
