603368b32cb01976293d470512048336e95fafcd
Piotr Solnica <piotr.solnica@gmail.com>
2011-07-22 13:58:15 +0200

    Add Typecast::TrueClass.to_string specs

---
+++ spec/unit/virtus/typecast/true_class/class_methods/to_string_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe Virtus::Typecast::TrueClass, '.to_string' do
+  subject { object.to_string(true_class) }
+
+  let(:object)     { described_class }
+  let(:true_class) { true           }
+
+  it { should be_instance_of(String) }
+
+  it { should eql('true') }
+end
