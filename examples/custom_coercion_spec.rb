4650badc5e248cd4db5a9a589f4b89165bd384b6
Piotr Solnica <piotr.solnica@gmail.com>
2011-07-26 12:02:34 +0200

    Add specs for examples in README

---
+++ examples/custom_coercion_spec.rb
@@ -0,0 +1,50 @@
+require './spec/spec_helper'
+
+class MD5 < Virtus::Attribute::Object
+  primitive       String
+  coercion_method :to_md5
+end
+
+module Virtus
+  class Coercion
+    class String < Virtus::Coercion::Object
+      def self.to_md5(value)
+        Digest::MD5.hexdigest(value)
+      end
+    end
+  end
+end
+
+class User
+  include Virtus
+
+  attribute :name,     String
+  attribute :password, MD5
+end
+
+describe User do
+  it { should respond_to(:name)      }
+  it { should respond_to(:name=)     }
+  it { should respond_to(:password)  }
+  it { should respond_to(:password=) }
+
+  describe '#name=' do
+    let(:value) { 'Piotr' }
+
+    before do
+      subject.name = value
+    end
+
+    its(:name) { should == value }
+  end
+
+  describe '#password=' do
+    let(:value) { 'foobar' }
+
+    before do
+      subject.password = value
+    end
+
+    its(:password) { should == Virtus::Coercion::String.to_md5(value) }
+  end
+end
