8cb58bba4bd75420163d6fbb1e4898a446c6164b
Piotr Solnica <piotr.solnica@gmail.com>
2011-07-28 12:07:17 +0200

    Remove Coercion::String.primitive method

--- lib/virtus/coercion/string.rb
+++ lib/virtus/coercion/string.rb
@@ -11,11 +11,6 @@ module Virtus
 
       NUMERIC_REGEXP = /\A(-?(?:0|[1-9]\d*)(?:\.\d+)?|(?:\.\d+))\z/.freeze
 
-      # @api private
-      def self.primitive
-        ::String
-      end
-
       # Coerce given value to Time
       #
       # @example
