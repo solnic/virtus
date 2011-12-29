8ff34bd1d562eaea1fb2331c22a7b9474d16020f
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2011-12-28 23:41:53 -0800

    Add unit specs for Attribute::Date#value_coerced?.

--- spec/unit/virtus/attribute/date_spec.rb
+++ spec/unit/virtus/attribute/date_spec.rb
@@ -56,4 +56,44 @@ describe Virtus::Attribute::Date do
       it { should equal(value) }
     end
   end
+
+  describe '#value_coerced?' do
+    let(:attribute) { described_class.new(:bday) }
+
+    let(:year)  { 2011 }
+    let(:month) { 4 }
+    let(:day)   { 7 }
+
+    subject { attribute.value_coerced?(value) }
+
+    context 'with a Date' do
+      let(:value) { Date.new(year, month, day) }
+      it { should be(true) }
+    end
+
+    context 'with a date time' do
+      let(:value) { DateTime.new(year, month, day) }
+      it { should be(true) }
+    end
+
+    context 'with a time' do
+      let(:value) { Time.local(year, month, day) }
+      it { should be(false) }
+    end
+
+    context 'with a hash' do
+      let(:value) { { :year => year, :month => month, :day => day } }
+      it { should be(false) }
+    end
+
+    context 'with a string' do
+      let(:value) { "April #{day}th, #{year}" }
+      it { should be(false) }
+    end
+
+    context 'with a non-date value' do
+      let(:value) { 'non-date' }
+      it { should be(false) }
+    end
+  end
 end
