f486059a2014da6272f65d0818986ee25beac691
Yves Senn <yves.senn@gmail.com>
2012-01-10 21:28:44 +0100

    Unify integration specs
    remove the existing integration specs
    
    integration spec for simple attribute definitions
    
    integration spec for default-values
    
    integration spec for method overrides
    
    integration spec for virtus classes with inheritance
    
    integration spec for embedded values
    
    remove all the constants in the Example-Module
    
    helper to remove constants after a test-run
    
    integration spec for custom attributes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,14 +17,23 @@ Dir[File.expand_path('../**/shared/**/*.rb', __FILE__)].each { |file| require fi
 
 RSpec.configure do |config|
 
-  # Remove anonymous Attribute classes from Attribute descendants
+  # Remove anonymous- and example- Attribute classes from Attribute descendants
   config.after :all do
     stack = [ Virtus::Attribute ]
     while klass = stack.pop
       klass.descendants.delete_if do |descendant|
-        descendant.name.nil? || descendant.name.empty?
+        descendant.name.nil? || descendant.name.empty? || descendant.name.start_with?('Examples::')
       end
       stack.concat(klass.descendants)
+    end
+  end
+
+  # Remove constants in the Example-Module
+  config.after :each do
+    if defined?(Examples)
+      Examples.constants.each do |const_name|
+        ConstantsHelpers.undef_constant(Examples, const_name)
+      end
     end
   end
 
