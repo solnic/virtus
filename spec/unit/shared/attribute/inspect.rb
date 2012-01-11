6cf51fa959af56fe2c900739890f46562726e6d0
Dan Kubb <dan.kubb@gmail.com>
2012-01-11 00:40:56 -0800

    Remove unused shared specs
    * Later on some of these specs will get added back as specs in the
      spec/unit/virtus/attribute directory

--- spec/unit/shared/attribute/inspect.rb
+++
@@ -1,7 +0,0 @@
-shared_examples_for 'Attribute#inspect' do
-  subject { attribute.inspect }
-
-  let(:attribute) { described_class.new(attribute_name) }
-
-  it { should == "#<#{described_class.inspect} @name=#{attribute_name.inspect}>" }
-end
