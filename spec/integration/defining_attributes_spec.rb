f52afb2e96ab57262c6ff25f4ecc62985f94c16a
Yves Senn <yves.senn@gmail.com>
2012-01-11 18:34:53 +0100

    integration spec for virtus classes with inheritance

--- spec/integration/defining_attributes_spec.rb
+++ spec/integration/defining_attributes_spec.rb
@@ -10,11 +10,16 @@ describe "virtus attribute definitions" do
         attribute :name, String
         attribute :age, Integer
         attribute :doctor, Boolean
+        attribute :salery, Decimal
+      end
+
+      class Manager < Person
+
       end
     end
   end
   after { Examples.send(:remove_const, 'Person') }
-
+  after { Examples.send(:remove_const, 'Manager') }
 
   subject { Examples::Person.new }
 
@@ -36,14 +41,41 @@ describe "virtus attribute definitions" do
   end
 
   context 'with attributes' do
-    subject { Examples::Person.new(:name => 'Jane', :age => 45, :doctor => true) }
+    let(:attributes) { {:name => 'Jane', :age => 45, :doctor => true, :salery => 4500} }
+    subject { Examples::Person.new(attributes) }
 
     specify "#attributes returns the object's attributes as a hash" do
-      subject.attributes.should == {:name => 'Jane', :age => 45, :doctor => true}
+      subject.attributes.should == attributes
     end
 
     specify "#to_hash returns the object's attributes as a hash" do
-      subject.to_hash.should == {:name => 'Jane', :age => 45, :doctor => true}
+      subject.to_hash.should == attributes
+    end
+  end
+
+  context 'inheritance' do
+    specify 'inherits all the attribtues from the base class' do
+      fred = Examples::Manager.new(:name => 'Fred', :age => 29)
+      fred.name.should == 'Fred'
+      fred.age.should == 29
+    end
+
+    specify 'lets you add attributes to the base class at runtime' do
+      frank = Examples::Manager.new(:name => 'Frank')
+      Examples::Person.attribute :just_added, String
+      frank.just_added = 'it works!'
+      frank.just_added.should == 'it works!'
+    end
+
+    specify 'lets you add attributes to the subclass at runtime' do
+      person_jack = Examples::Person.new(:name => 'Jack')
+      manager_frank = Examples::Manager.new(:name => 'Frank')
+      Examples::Manager.attribute :just_added, String
+
+      manager_frank.just_added = 'awesome!'
+      manager_frank.just_added.should == 'awesome!'
+      person_jack.should_not respond_to(:just_added)
+      person_jack.should_not respond_to(:just_added=)
     end
   end
 end
