1deea7e527a46bba592fd32780c8e92f99fdacfc
Yves Senn <yves.senn@gmail.com>
2012-01-11 20:02:33 +0100

    helper to remove constants after a test-run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,12 +17,12 @@ Dir[File.expand_path('../**/shared/**/*.rb', __FILE__)].each { |file| require fi
 
 RSpec.configure do |config|
 
-  # Remove anonymous Attribute classes from Attribute descendants
+  # Remove anonymous- and example- Attribute classes from Attribute descendants
   config.after :all do
     stack = [ Virtus::Attribute ]
     while klass = stack.pop
       klass.descendants.delete_if do |descendant|
-        descendant.name.nil? || descendant.name.empty?
+        descendant.name.nil? || descendant.name.empty? || descendant.name.start_with?('Examples::')
       end
       stack.concat(klass.descendants)
     end
@@ -31,8 +31,8 @@ RSpec.configure do |config|
   # Remove constants in the Example-Module
   config.after :each do
     if defined?(Examples)
-      Examples.constants.each do |constant|
-        Examples.send(:remove_const, constant)
+      Examples.constants.each do |const_name|
+        ConstantsHelpers.undef_constant(Examples, const_name)
       end
     end
   end
