dab3586d5a437a4e77ddb9ed62f9b71961f46566
Dan Kubb <dan.kubb@gmail.com>
2012-01-12 00:59:59 -0800

    Update Virtus::InstanceMethods#attributes= specs to catch more mutations

--- spec/unit/virtus/instance_methods/attributes_spec.rb
+++ spec/unit/virtus/instance_methods/attributes_spec.rb
@@ -13,11 +13,12 @@ describe Virtus::InstanceMethods do
     end
   end
 
-  let(:object)     { model.new(attributes)           }
   let(:attributes) { { :name => 'john', :age => 28 } }
 
   describe '#attributes' do
     subject { object.attributes }
+
+    let(:object) { model.new(attributes) }
 
     it { should be_instance_of(Hash) }
 
@@ -25,53 +26,71 @@ describe Virtus::InstanceMethods do
   end
 
   describe '#attributes=' do
-    subject { object.attributes = attribute_values; object }
+    subject { object.attributes = attribute_values }
 
-    let(:object) { model.new({}) }
+    let(:object) { model.new }
 
     context 'when given values for publicly writable attributes' do
       let(:attribute_values) { attributes }
 
+      it { should be(attribute_values) }
+
       it 'sets all provided attribute values' do
-        subject.attributes.should eql(attributes)
+        object.attributes.should eql(:age => nil, :name => nil)
+        subject
+        object.attributes.should eql(attributes)
       end
     end
 
     context 'when given string keys for attributes' do
-      let(:attribute_values) do
-        { 'name' => 'john', 'age' => 28 }
-      end
+      let(:attribute_values) { { 'name' => 'john', 'age' => 28 } }
+
+      it { should be(attribute_values) }
 
       it 'sets values for publicly accessible attributes' do
-        subject.attributes.should eql(attributes)
+        object.attributes.should eql(:age => nil, :name => nil)
+        subject
+        object.attributes.should eql(attributes)
       end
     end
 
     context 'when given values for privately writable attributes' do
       let(:attribute_values) { attributes.merge(:email => 'john@domain.tld') }
 
+      it { should be(attribute_values) }
+
       it 'only sets values for publicly accessible attributes' do
-        subject.attributes.should eql(attributes)
+        object.attributes.should eql(:age => nil, :name => nil)
+        subject
+        object.attributes.should eql(attributes)
       end
     end
 
     context 'when given values for non-attribute setters' do
       let(:attribute_values) { attributes.merge(:non_attribute => 'foobar') }
 
+      it { should be(attribute_values) }
+
       it 'sets values for publicly accessible attributes' do
-        subject.attributes.should eql(attributes)
+        object.attributes.should eql(:age => nil, :name => nil)
+        subject
+        object.attributes.should eql(attributes)
       end
 
       it 'silently ignores publicly accessible non-Virtus attributes' do
-        subject.non_attribute.should be_nil
+        expect { subject }.to_not change { object.non_attribute }
       end
     end
 
     context "when given values that don't correspond to attributes" do
       let(:attribute_values) { attributes.merge(:quux => 'foobar') }
 
+      it { should be(attribute_values) }
+
       it 'sets values for publicly accessible attributes' do
-        subject.attributes.should eql(attributes)
+        object.attributes.should eql(:age => nil, :name => nil)
+        subject
+        object.attributes.should eql(attributes)
       end
 
       it 'silently ignores non-attribute keys' do
