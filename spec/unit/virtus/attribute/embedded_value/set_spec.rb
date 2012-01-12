8ff9f25d8416e0ee160330674749cabeff5427a3
Dan Kubb <dan.kubb@gmail.com>
2012-01-11 21:30:21 -0800

    Add specs for case where model is not explicitly passed in to EV constructor

--- spec/unit/virtus/attribute/embedded_value/set_spec.rb
+++ spec/unit/virtus/attribute/embedded_value/set_spec.rb
@@ -1,30 +1,64 @@
 require 'spec_helper'
 
 describe Virtus::Attribute::EmbeddedValue, '#set' do
-  subject { attribute.set(instance, value) }
+  subject { object.set(instance, value) }
 
-  let(:embedded_model)         { OpenStruct                                                    }
-  let(:attribute_name)         { :address                                                      }
-  let(:attribute_value)        { model.new                                                     }
-  let(:attribute_value_other)  { model.new                                                     }
-  let(:attribute_default)      { {}                                                            }
-  let(:attribute_default_proc) { lambda { |instance, attribute| attribute.name == :address }   }
-  let(:attribute)              { described_class.new(attribute_name, :model => embedded_model) }
-  let(:model)                  { Class.new                                                     }
-  let(:instance)               { model.new                                                     }
+  let(:attribute_name) { :attribute_name }
+  let(:model)          { OpenStruct      }
+  let(:instance)       { Object.new      }
 
-  context 'with a hash' do
-    let(:value)                   { Hash.new(:foo => 'bar')         }
-    let(:embedded_model_instance) { mock('embedded_model_instance') }
+  context 'when the value is a hash' do
+    let(:value)          { { :foo => 'bar' }      }
+    let(:model_instance) { mock('model_instance') }
 
     before do
-      embedded_model.should_receive(:new).with(value).and_return(embedded_model_instance)
-      subject
+      model.should_receive(:new).with(value).and_return(model_instance)
     end
 
-    it 'creates an embedded value instance with attributes' do
-      instance.instance_variable_get(attribute.instance_variable_name).
-        should be(embedded_model_instance)
+    context 'when the options include the model' do
+      let(:object) { described_class.new(attribute_name, :model => model) }
+      let(:model)  { mock('model')                                        }
+
+      it 'sets the embedded value instance' do
+        object.get(instance).should be_nil
+        subject
+        object.get(instance).should be(model_instance)
+      end
+    end
+
+    context 'when the options are an empty Hash' do
+      let(:object) { described_class.new(attribute_name, {}) }
+
+      it 'sets the embedded model instance within the instance' do
+        object.get(instance).should be_nil
+        subject
+        object.get(instance).should be(model_instance)
+      end
+    end
+
+    context 'when the options are not provided' do
+      let(:object) { described_class.new(attribute_name) }
+
+      it 'sets the embedded model instance within the instance' do
+        object.get(instance).should be_nil
+        subject
+        object.get(instance).should be(model_instance)
+      end
+    end
+  end
+
+  context 'when the value is not a hash' do
+    let(:object) { described_class.new(attribute_name) }
+    let(:value)  { mock('value')                       }
+
+    before do
+      model.should_not_receive(:new)
+    end
+
+    it 'sets the value in the instance' do
+      object.get(instance).should be_nil
+      subject
+      object.get(instance).should be(value)
     end
   end
 end
