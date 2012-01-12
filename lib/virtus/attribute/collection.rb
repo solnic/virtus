6c9037d76fda8af283ae06029c634c406b178f03
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2012-01-12 01:26:04 -0800

    Always set :member_type key in Attribute::Collection.merge_options.

--- lib/virtus/attribute/collection.rb
+++ lib/virtus/attribute/collection.rb
@@ -11,19 +11,16 @@ module Virtus
       attr_reader :member_type
 
       def self.merge_options(type, options)
-        if !type.respond_to?(:size) || type.size == 0
-          options
-        elsif type.size == 1
-          options.merge(:member_type => type.first)
-        elsif type.size > 1
-          raise NotImplementedError,
-            "build SumType from list of types (#{type.inspect})"
+        if type.size > 1
+          raise NotImplementedError, "build SumType from list of types (#{type.inspect})"
+        else
+          options.merge(:member_type => type.first || Virtus::Attribute::Object)
         end
       end
 
       def initialize(*)
         super
-        @member_type = @options.fetch(:member_type, Virtus::Attribute::Object)
+        @member_type = @options.fetch(:member_type)
         @member_type_instance = Attribute.build(@name, @member_type)
       end
 
