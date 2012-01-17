7a9512b3c63ab7097b7c024a2d0770e466417a40
Dan Kubb <dan.kubb@gmail.com>
2012-01-16 21:17:15 -0800

    Add spec for Virtus::Attribute.accessor

---
+++ spec/unit/virtus/attribute/class_methods/accessor_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Virtus::Attribute, '.accessor' do
+  let(:object)          { Class.new(described_class)                                       }
+  let(:options)         { { :primitive => primitive, :coercion_method => coercion_method } }
+  let(:primitive)       { stub('primitive')                                                }
+  let(:coercion_method) { stub('coercion_method')                                          }
+
+  context 'with no argument' do
+    subject { object.accessor } 
+
+    it { should be_nil }
+  end
+
+  context 'with the default accessor' do
+    subject { object.accessor(value) }
+
+    let(:value) { stub('value') }
+
+    it { should equal(object) } 
+
+    it 'sets the accessor default for the class' do                                        
+      expect { subject }.to change { object.accessor }.from(nil).to(value)                 
+    end
+  end
+end
