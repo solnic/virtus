bb9ab9170420e9101a35f23b04aae17aac942ecd
Dan Kubb <dan.kubb@gmail.com>
2012-01-21 22:26:06 -0800

    Add spec for Virtus::Attribute#set

--- spec/unit/virtus/attribute/set_spec.rb
+++ spec/unit/virtus/attribute/set_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
+describe Virtus::Attribute, '#set' do
+  subject { object.set(instance, value) }
+
+  let(:object)          { described_class.new(:name, options)                              }
+  let(:options)         { { :primitive => primitive, :coercion_method => coercion_method } }
+  let(:primitive)       { stub('primitive')                                                }
+  let(:coercion_method) { stub('coercion_method')                                          }
+  let(:instance)        { Object.new                                                       }
+  let(:value)           { stub('value')                                                    }
+  let(:coerced)         { stub('coerced')                                                  }
+
+  before do
+    object.stub(:coerce).with(value).and_return(coerced)
+    object.stub(:set!).with(instance, coerced).and_return(object)
+  end
+
+  it { should be(object) }
+
+  it 'coerces the value' do
+    object.should_receive(:coerce).with(value)
+    subject
+  end
+
+  it 'sets the instance variable to the coerced value' do
+    object.should_receive(:set!).with(instance, coerced)
+    subject
+  end
+end
+
 describe Virtus::Attribute, '#set!' do
   subject { object.set!(instance, value) }
 
