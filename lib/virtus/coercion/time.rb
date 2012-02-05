d291c31f31aa03c6c9f3a7f18becb311980ce809
Piotr Solnica <piotr.solnica@gmail.com>
2012-02-06 00:46:19 +0100

    Add Time.to_integer coercion

--- lib/virtus/coercion/time.rb
+++ lib/virtus/coercion/time.rb
@@ -21,6 +21,20 @@ module Virtus
         value
       end
 
+      # Creates a Fixnum instance from a Time object
+      #
+      # @example
+      #   Virtus::Coercion::Time.to_integer(time)  # => Fixnum object
+      #
+      # @param [Time] value
+      #
+      # @return [Fixnum]
+      #
+      # @api public
+      def self.to_integer(value)
+        value.to_i
+      end
+
     end # class Time
   end # class Coercion
 end # module Virtus
