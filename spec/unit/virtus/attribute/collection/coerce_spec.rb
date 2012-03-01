894c3277164a1527b729e314478f3eac3eaad856
Dan Kubb <dan.kubb@gmail.com>
2012-02-29 21:05:30 -0800

    Add specs for Virtus::Attribute::Collection#coerce
    * The existing specs only tested two out of 3 possible states the method could
      exist in. Add another spec to cover the actual "happy path" when
      #coerce_and_append_member is implemented for the Collection subclass.

--- spec/unit/virtus/attribute/collection/coerce_spec.rb
+++ spec/unit/virtus/attribute/collection/coerce_spec.rb
@@ -1,26 +1,39 @@
 require 'spec_helper'
 
 describe Virtus::Attribute::Collection, '#coerce' do
-  subject { attribute.coerce(value) }
+  subject { object.coerce(value) }
 
-  let(:attribute)   { Class.new(described_class).new(:things, :member_type => member_type) }
-  let(:value)       { [ entry ] }
-  let(:entry)       { mock('entry') }
-  let(:member_type) { Virtus::Attribute::Object }
+  let(:described_class) { Class.new(Virtus::Attribute::Collection) }
+  let(:object)          { described_class.new(:things) }
+  let(:value)           { [ entry ] }
+  let(:entry)           { mock('entry') }
 
   before do
+    described_class.primitive ::Array
     value.should_receive(:respond_to?).with(:inject).and_return(respond_to_inject)
   end
 
   context 'when coerced value responds to #inject' do
     let(:respond_to_inject) { true }
 
-    specify { expect { subject }.to raise_error(NotImplementedError) }
+    context 'when the object has not implemented #coerce_and_append_member' do
+      specify { expect { subject }.to raise_error(NotImplementedError, "#{object.class}#coerce_and_append_member has not been implemented") }
+    end
+
+    context 'when the object has implemented #coerce_and_append_member' do
+      before do
+        def object.coerce_and_append_member(collection, entry)
+          collection << entry
+        end
+      end
+
+      it { should eql(value) }
+    end
   end
 
   context 'when coerced value does not respond to #inject' do
     let(:respond_to_inject) { false }
 
-    specify { should eql(value) }
+    it { should eql(value) }
   end
 end
