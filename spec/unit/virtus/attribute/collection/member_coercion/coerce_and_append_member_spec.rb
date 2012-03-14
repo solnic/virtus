aaa9156ca9e169c2d65ba7766ff2ab2e0c7d4480
Piotr Solnica <piotr.solnica@gmail.com>
2012-03-14 12:23:04 +0100

    Add spec for Virtus::Attribute::Collection::MemberCoercion#coerce_and_append_member

--- spec/unit/virtus/attribute/collection/member_coercion/coerce_and_append_member_spec.rb
+++ spec/unit/virtus/attribute/collection/member_coercion/coerce_and_append_member_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Virtus::Attribute::Collection::MemberCoercion, '#coerce_and_append_member' do
-  it 'should have spec'
+  subject { object.coerce_and_append_member(collection, entry) }
+
+  let(:attribute_class) do
+    Class.new(Virtus::Attribute::Collection) do
+      include Virtus::Attribute::Collection::MemberCoercion
+      self
+    end
+  end
+
+  let(:object)     { attribute_class.new('stuff', :member_type => Integer) }
+  let(:collection) { []  }
+  let(:entry)      { '1' }
+
+  it { should eql([ 1 ]) }
 end
