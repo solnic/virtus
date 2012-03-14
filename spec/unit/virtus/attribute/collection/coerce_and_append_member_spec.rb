5eefa9e584ca8d128a34602e26cd21c08c8cea56
Piotr Solnica <piotr.solnica@gmail.com>
2012-03-14 12:11:44 +0100

    Add spec for Virtus::Attribute::Collection#coerce_and_append_member

--- spec/unit/virtus/attribute/collection/coerce_and_append_member_spec.rb
+++ spec/unit/virtus/attribute/collection/coerce_and_append_member_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Virtus::Attribute::Collection, '#coerce_and_append_member' do
-  it 'should have spec'
+  subject { object.coerce_and_append_member(collection, entry) }
+
+  let(:object)     { described_class.new('stuff') }
+  let(:collection) { mock('collection')           }
+  let(:entry)      { mock('entry')                }
+
+  specify do
+    expect { subject }.to raise_error(
+      NotImplementedError,
+      "Virtus::Attribute::Collection#coerce_and_append_member has not been implemented"
+    )
+  end
 end
