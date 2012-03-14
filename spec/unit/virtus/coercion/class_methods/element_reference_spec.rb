2db8c30fa280c1730d36a75056427bb8620d5c6c
Piotr Solnica <piotr.solnica@gmail.com>
2012-03-14 12:03:33 +0100

    Move Coercion.[] spec to correct file

--- spec/unit/virtus/coercion/class_methods/element_reference_spec.rb
+++ spec/unit/virtus/coercion/class_methods/element_reference_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
-describe Virtus::Coercion, '.[]' do
-  it 'should have spec'
+describe Virtus::Coercion, '#[]' do
+  %w[ Decimal Date DateTime FalseClass Integer Float Hash String Symbol Time TrueClass ].each do |class_name|
+    context "with #{class_name.inspect}" do
+      subject { described_class[class_name] }
+
+      it { should == described_class.const_get(class_name) }
+    end
+  end
+
+  context 'with a name of a class not defined in Coercion' do
+    subject { described_class['Set'] }
+
+    it { should == Virtus::Coercion::Object }
+  end
 end
