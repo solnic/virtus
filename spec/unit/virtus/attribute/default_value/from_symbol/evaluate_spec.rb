152485016ed10797d3394c629230ada900fd4c27
Piotr Solnica <piotr.solnica@gmail.com>
2012-03-15 15:08:04 +0100

    Add a spec for Virtus::Attribute::DefaultValue::FromSymbol#evaluate

--- spec/unit/virtus/attribute/default_value/from_symbol/evaluate_spec.rb
+++ spec/unit/virtus/attribute/default_value/from_symbol/evaluate_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Virtus::Attribute::DefaultValue::FromSymbol, '#evaluate' do
-  it 'should have spec'
+  subject { object.evaluate(instance) }
+
+  let(:object)    { described_class.new(attribute, value) }
+  let(:attribute) { mock('attribute')                       }
+  let(:instance)  { mock('instance', value => retval)       }
+  let(:value)     { :set_default                            }
+  let(:retval)    { mock('retval')                          }
+
+  it { should be(retval) }
+
+  it 'calls the method' do
+    instance.should_receive(value).with(no_args)
+    subject
+  end
 end
