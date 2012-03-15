eeb6418a6297c3a984b205f3b28b63ff8300a49e
Piotr Solnica <piotr.solnica@gmail.com>
2012-03-15 14:54:53 +0100

    Add spec for Virtus::Attribute::DefaultValue::FromClonable.handle?

--- spec/unit/virtus/attribute/default_value/from_clonable/class_methods/handle_spec.rb
+++ spec/unit/virtus/attribute/default_value/from_clonable/class_methods/handle_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
-describe Virtus::Attribute::DefaultValue::FromClonable, '.handle' do
-  it 'should have spec'
+describe Virtus::Attribute::DefaultValue::FromClonable, '.handle?' do
+  subject { described_class.handle?(attribute, default) }
+
+  let(:attribute) { mock('attribute') }
+
+  context 'with a clonable' do
+    let(:default) { 'clonable'  }
+
+    it { should be(true)  }
+  end
+
+  context 'with a non-clonable' do
+    let(:default) { 1 }
+
+    it { should be(false)  }
+  end
 end
+
