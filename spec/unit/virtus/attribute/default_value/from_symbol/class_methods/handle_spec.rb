0074019b7510b9227c9a13133de5f4aa5cb5c8ef
Piotr Solnica <piotr.solnica@gmail.com>
2012-03-15 14:56:36 +0100

    Add spec for Virtus::Attribute::DefaultValue::FromSymbol.handle?

--- spec/unit/virtus/attribute/default_value/from_symbol/class_methods/handle_spec.rb
+++ spec/unit/virtus/attribute/default_value/from_symbol/class_methods/handle_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
-describe Virtus::Attribute::DefaultValue::FromSymbol, '.handle' do
-  it 'should have spec'
+describe Virtus::Attribute::DefaultValue::FromSymbol, '.handle?' do
+  subject { described_class.handle?(attribute, default) }
+
+  let(:attribute) { mock('attribute') }
+
+  context 'with a symbol' do
+    let(:default) { :method_name  }
+
+    it { should be(true)  }
+  end
+
+  context 'with a non-symbol' do
+    let(:default) { 'method_name' }
+
+    it { should be(false)  }
+  end
 end
+
