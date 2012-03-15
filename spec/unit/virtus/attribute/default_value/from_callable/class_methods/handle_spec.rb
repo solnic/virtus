56fa1793ce803f0825800ac8567b8a1c589048fb
Piotr Solnica <piotr.solnica@gmail.com>
2012-03-15 14:53:33 +0100

    Add spec for Virtus::Attribute::DefaultValue::FromCallable.handle?

--- spec/unit/virtus/attribute/default_value/from_callable/class_methods/handle_spec.rb
+++ spec/unit/virtus/attribute/default_value/from_callable/class_methods/handle_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
-describe Virtus::Attribute::DefaultValue::FromCallable, '.handle' do
-  it 'should have spec'
+describe Virtus::Attribute::DefaultValue::FromCallable, '.handle?' do
+  subject { described_class.handle?(attribute, default) }
+
+  let(:attribute) { mock('attribute') }
+
+  context 'with a callable' do
+    let(:default) { Proc.new {}  }
+
+    it { should be(true)  }
+  end
+
+  context 'with a non-callable' do
+    let(:default) { '' }
+
+    it { should be(false)  }
+  end
 end
+
