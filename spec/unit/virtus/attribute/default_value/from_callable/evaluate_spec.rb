de97110ba23c92e4521e5c26ce3a324b29dbe950
Piotr Solnica <piotr.solnica@gmail.com>
2012-03-15 15:04:13 +0100

    Add a spec for Virtus::Attribute::DefaultValue::FromCallable#evaluate

--- spec/unit/virtus/attribute/default_value/from_callable/evaluate_spec.rb
+++ spec/unit/virtus/attribute/default_value/from_callable/evaluate_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Virtus::Attribute::DefaultValue::FromCallable, '#evaluate' do
-  it 'should have spec'
+  subject { object.evaluate(instance) }
+
+  let(:object)    { described_class.new(attribute, value) }
+  let(:attribute) { mock('attribute')                     }
+  let(:value)     { mock('value')                         }
+  let(:instance)  { mock('instance')                      }
+  let(:response)  { stub('response')                      }
+
+  before { value.stub(:call => response) }
+
+  specify { object.should be_instance_of(Virtus::Attribute::DefaultValue::FromCallable) }
+
+  it { should be(response) }
+
+  it 'calls the value with the instance and attribute' do
+    value.should_receive(:call).with(instance, attribute)
+    subject
+  end
 end
+
