732097186133d28b84458df7971a83a49df33f74
Piotr Solnica <piotr.solnica@gmail.com>
2012-03-15 14:30:46 +0100

    Add spec for Virtus::Attribute::DefaultValue.build

--- spec/unit/virtus/attribute/default_value/class_methods/build_spec.rb
+++ spec/unit/virtus/attribute/default_value/class_methods/build_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Virtus::Attribute::DefaultValue, '.build' do
-  it 'should have spec'
+  subject { described_class.build(attribute, default)  }
+
+  let(:attribute) { mock('attribute')  }
+
+  context 'when default is a symbol' do
+    let(:default) { :symbol }
+
+    it { should be_instance_of(Virtus::Attribute::DefaultValue::FromSymbol)  }
+  end
+
+  context 'when default is a callable' do
+    let(:default) { Proc.new {} }
+
+    it { should be_instance_of(Virtus::Attribute::DefaultValue::FromCallable)  }
+  end
+
+  context 'when default is a clonable' do
+    let(:default) { "I can be cloned" }
+
+    it { should be_instance_of(Virtus::Attribute::DefaultValue::FromClonable)  }
+  end
 end
