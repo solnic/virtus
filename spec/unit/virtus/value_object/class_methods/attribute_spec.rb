9cc4918b0ae3cd38c2b22a061132a634002ff0e9
Piotr Solnica <piotr.solnica@gmail.com>
2012-03-19 13:02:20 +0100

    Add spec for Virtus::ValueObject.attribute

--- spec/unit/virtus/value_object/class_methods/attribute_spec.rb
+++ spec/unit/virtus/value_object/class_methods/attribute_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Virtus::ValueObject, '.attribute' do
-  it 'should have spec'
+  subject { model.attributes[name] }
+
+  let(:model) { Class.new { include Virtus::ValueObject } }
+  let(:name)  { :lat  }
+  let(:type)  { Float }
+
+  before { model.attribute(name, type) }
+
+  it { should be_instance_of(Virtus::Attribute::Float) }
+
+  specify { subject.options[:writer].should be(:private) }
 end
+
