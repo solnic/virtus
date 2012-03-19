721d57894295b0140b6f058cff483c14ffc0558d
Piotr Solnica <piotr.solnica@gmail.com>
2012-03-19 12:41:12 +0100

    Add spec for Virtus::TypeLookup.extended

--- spec/unit/virtus/type_lookup/class_methods/extended_spec.rb
+++ spec/unit/virtus/type_lookup/class_methods/extended_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe Virtus::TypeLookup, '.extended' do
-  it 'should have spec'
+  let(:type) { Class.new { extend Virtus::TypeLookup } }
+
+  it 'sets type_lookup_catch ivar' do
+    type.instance_variable_get('@type_lookup_cache').should eql({})
+  end
 end
+
