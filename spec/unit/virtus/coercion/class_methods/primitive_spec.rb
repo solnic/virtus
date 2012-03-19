22bdc713b5893fe76e02db1c5e8fc08060464fca
Piotr Solnica <piotr.solnica@gmail.com>
2012-03-19 12:27:03 +0100

    Add specs for Virtus::Coercion.primitive

--- spec/unit/virtus/coercion/class_methods/primitive_spec.rb
+++ spec/unit/virtus/coercion/class_methods/primitive_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Virtus::Coercion, '.primitive' do
-  it 'should have spec'
+  Virtus::Coercion.descendants.each do |descendant|
+    subject { descendant.primitive }
+
+    let(:primitive) { Object.const_get(descendant.name.split('::').last.to_sym) }
+
+    describe "#{descendant}.primitive" do
+      it { should be(primitive) }
+    end
+  end
 end
