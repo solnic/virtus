1f56d587e0bf93f78fefd4fb859866f2d1cd7d84
Piotr Solnica <piotr.solnica@gmail.com>
2012-03-22 11:24:41 +0100

    Use real float object in Virtus::Coercion::Float.to_float

--- spec/unit/virtus/coercion/float/class_methods/to_float_spec.rb
+++ spec/unit/virtus/coercion/float/class_methods/to_float_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Virtus::Coercion::Float, '.to_float' do
   subject { described_class.to_float(value) }
 
-  let(:value) { mock('value') }
+  let(:value) { 1.0 }
 
   it { should be(value) }
 end
