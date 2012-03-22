a867738d7b057e3e8c15d0e9a09e74515d8a2b96
Piotr Solnica <piotr.solnica@gmail.com>
2012-03-22 11:22:03 +0100

    Use real decimal object in Virtus::Coercion::Decimal.to_decimal

--- spec/unit/virtus/coercion/decimal/class_methods/to_decimal_spec.rb
+++ spec/unit/virtus/coercion/decimal/class_methods/to_decimal_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Virtus::Coercion::Decimal, '.to_decimal' do
   subject { described_class.to_decimal(value) }
 
-  let(:value) { mock('value') }
+  let(:value) { BigDecimal('1.0') }
 
   it { should be(value) }
 end
