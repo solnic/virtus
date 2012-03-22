b8ca28cd6cfa80688ad5989b9a0b6fffef155708
Piotr Solnica <piotr.solnica@gmail.com>
2012-03-22 11:46:45 +0100

    Use real integer object in Virtus::Coercion::Integer.to_integer spec

--- spec/unit/virtus/coercion/integer/class_methods/to_integer_spec.rb
+++ spec/unit/virtus/coercion/integer/class_methods/to_integer_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Virtus::Coercion::Integer, '.to_integer' do
   subject { described_class.to_integer(value) }
 
-  let(:value) { mock('value') }
+  let(:value) { 1 }
 
   it { should be(value) }
 end
