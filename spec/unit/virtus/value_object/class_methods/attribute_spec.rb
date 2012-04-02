dc8c0311e4a6be0880669035ef380079b24f4083
Dan Kubb <dan.kubb@gmail.com>
2012-04-02 00:55:28 -0700

    Refactor Virtus::ValueObject::ClassMethods#attribute specs
    * Include matcher for equalizer setuop
    * Clean up specs to match conventions elsewhere

--- spec/unit/virtus/value_object/class_methods/attribute_spec.rb
+++ spec/unit/virtus/value_object/class_methods/attribute_spec.rb
@@ -1,15 +1,23 @@
 require 'spec_helper'
 
 describe Virtus::ValueObject, '.attribute' do
-  subject { model.attributes[name] }
+  subject { object.attribute(name, type) }
 
-  let(:model) { Class.new { include Virtus::ValueObject } }
-  let(:name)  { :lat  }
-  let(:type)  { Float }
+  let(:object)    { Class.new { include Virtus::ValueObject } }
+  let(:name)      { :latitude                                 }
+  let(:type)      { Float                                     }
+  let(:attribute) { object.attributes[name]                   }
 
-  before { model.attribute(name, type) }
+  it { should be(object) }
 
-  it { should be_instance_of(Virtus::Attribute::Float) }
+  it 'adds the attribute to the equalizer' do
+    object.new.inspect.should_not match(/\b#{name}=\b/)
+    subject
+    object.new.inspect.should match(/\b#{name}=\b/)
+  end
 
-  specify { subject.options[:writer].should be(:private) }
+  it 'sets the writer to be private' do
+    subject
+    attribute.options[:writer].should be(:private)
+  end
 end
