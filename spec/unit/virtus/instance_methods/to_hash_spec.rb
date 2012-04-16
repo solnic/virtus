3bfc894b3e248f388cce492dd18dfcd1c4d8dc89
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2012-04-16 20:10:35 +0200

    Adding recursive #to_hash implementation and spec.
    Makes API more consistent. Now, if record was created from
    some hash, it will get serialized back to same hash.
    
    Closes #82.

--- spec/unit/virtus/instance_methods/to_hash_spec.rb
+++ spec/unit/virtus/instance_methods/to_hash_spec.rb
@@ -3,17 +3,29 @@ require 'spec_helper'
 describe Virtus::InstanceMethods, '#to_hash' do
   subject { object.to_hash }
 
-  class Model
+  class Address
     include Virtus
 
-    attribute :name,  String
-    attribute :age,   Integer
-    attribute :email, String, :accessor => :private
+    attribute :street,  String
+    attribute :city,    String
   end
 
-  let(:model)      { Model                           }
-  let(:object)     { model.new(attributes)           }
-  let(:attributes) { { :name => 'john', :age => 28 } }
+  class Person
+    include Virtus
+
+    attribute :name,    String
+    attribute :age,     Integer
+    attribute :email,   String, :accessor => :private
+    attribute :address, Address
+  end
+
+  let(:model)        { Person                          }
+  let(:child_record) { Address                         }
+
+  let(:address)    { { :street => "Sunshinestr.", :city => "Berlin" } }
+  let(:object)     { model.new(attributes)                            }
+
+  let(:attributes) { { :name => 'john', :age => 28, :address => child_record } }
 
   it { should be_instance_of(Hash) }
 
