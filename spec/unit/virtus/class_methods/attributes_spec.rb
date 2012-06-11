afcb223dd3c4b10b9582d0da0a31db20f2fdd7bb
Dan Kubb <dan.kubb@gmail.com>
2012-06-11 13:27:33 -0700

    Fix spec to pass under ruby 1.9

--- spec/unit/virtus/class_methods/attributes_spec.rb
+++ spec/unit/virtus/class_methods/attributes_spec.rb
@@ -16,8 +16,7 @@ describe Virtus::ClassMethods, '#attributes' do
   it { should be_instance_of(Virtus::AttributeSet) }
 
   it 'returns a deprecation warning' do
-    lambda { subject }.should change { $stderr.string.dup }.from('').to(
-      "#{object}.attributes is deprecated. Use #{object}.attribute_set instead: #{__FILE__}:4\n"
-    )
+    subject
+    $stderr.string.should =~ /\A#{object}.attributes is deprecated. Use #{object}.attribute_set instead: #{__FILE__}:4\b/
   end
 end
