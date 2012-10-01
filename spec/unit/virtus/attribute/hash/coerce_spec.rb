bd5f2e9de5b20436d52a59a814c1966737e6ceb1
Piotr Solnica <piotr.solnica@gmail.com>
2012-10-01 11:50:58 +0300

    Minor formatting fixes in Hash#coerce spec

--- spec/unit/virtus/attribute/hash/coerce_spec.rb
+++ spec/unit/virtus/attribute/hash/coerce_spec.rb
@@ -3,18 +3,18 @@ require 'spec_helper'
 describe Virtus::Attribute::Hash, '#coerce' do
   subject { object.coerce(input_value) }
 
-  let(:options) { {:key_type => String, :value_type => Float } }
+  let(:options) { { :key_type => String, :value_type => Float } }
   let(:object)  { Virtus::Attribute::Hash.new('stuff', options) }
 
   context 'respond to `inject`' do
     let(:input_value) { { :one => '1', 'two' => 2 } }
 
-    it { should == { 'one' => 1.0, 'two' => 2.0 } }
+    it { should eql('one' => 1.0, 'two' => 2.0) }
   end
 
   context 'does not respond to `inject`' do
     let(:input_value) { :symbol }
 
-    it { should == :symbol }
+    it { should be(:symbol) }
   end
 end
