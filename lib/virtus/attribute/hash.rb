d9577ee225e83e5dda6c3332b832184a5c566d3d
Cyril Mougel <cyril.mougel@gmail.com>
2012-12-18 09:44:47 +0100

    Fix the Hash coercion failure if you don't define a complete Hash Coercion
    Since virtus 0.5.3 a new Coercion Hash was introduce like
    Hash[Symbol => Float]
    
    But with old Hash system there are a failure. This commit fix that.

--- lib/virtus/attribute/hash.rb
+++ lib/virtus/attribute/hash.rb
@@ -94,10 +94,20 @@ module Virtus
         coerced = super
         return coerced unless coerced.respond_to?(:each_with_object)
         coerced.each_with_object({}) do |key_and_value, hash|
-          key   = @key_type_instance.coerce(key_and_value[0])
-          value = @value_type_instance.coerce(key_and_value[1])
+          key   = coerce_key(key_and_value[0])
+          value = coerce_value(key_and_value[1])
           hash[key] = value
         end
+      end
+
+      private
+
+      def coerce_key(key)
+        @key_type_instance ? @key_type_instance.coerce(key) : key
+      end
+
+      def coerce_value(value)
+        @value_type_instance ? @value_type_instance.coerce(value) : value
       end
 
     end # class Hash
