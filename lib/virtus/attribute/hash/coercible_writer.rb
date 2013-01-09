b9f561e5a6b1edb084d51b5febdcff56e37d0d22
Piotr Solnica <piotr.solnica@gmail.com>
2013-01-09 11:12:45 +0100

    Add docs for Hash::CoercibleWriter

--- lib/virtus/attribute/hash/coercible_writer.rb
+++ lib/virtus/attribute/hash/coercible_writer.rb
@@ -2,44 +2,36 @@ module Virtus
   class Attribute
     class Hash
 
+      # Coercible writer for Hash attributes
+      #
       class CoercibleWriter < Attribute::Writer::Coercible
 
         # The type to which keys of this hash will be coerced
         #
-        # @example
-        #
-        #   class Request
-        #     include Virtus
-        #
-        #     attribute :headers, Hash[Symbol => String]
-        #   end
-        #
-        #   Post.attributes[:headers].key_type # => Virtus::Attribute::Symbol
-        #
         # @return [Virtus::Attribute]
         #
-        # @api public
+        # @api private
         attr_reader :key_type
 
         # The type to which values of this hash will be coerced
         #
-        # @example
-        #
-        #   class Request
-        #     include Virtus
-        #
-        #     attribute :headers, Hash[Symbol => String]
-        #   end
-        #
-        #   Post.attributes[:headers].value_type # => Virtus::Attribute::String
-        #
         # @return [Virtus::Attribute]
         #
-        # @api public
+        # @api private
         attr_reader :value_type
 
+        # Return writer for hash keys
+        #
+        # @return [Writer::Coercible]
+        #
+        # @api private
         attr_reader :key_writer
 
+        # Return writer for hash values
+        #
+        # @return [Writer::Coercible]
+        #
+        # @api private
         attr_reader :value_writer
 
         # @api private
