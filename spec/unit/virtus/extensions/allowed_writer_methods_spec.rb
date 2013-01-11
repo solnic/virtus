ef40c5338322206f28688a3a7565283953be06bb
Piotr Solnica <piotr.solnica@gmail.com>
2013-01-11 11:28:27 +0100

    Add missing spec for Extensions#allowed_writer_methods

--- spec/unit/virtus/extensions/allowed_writer_methods_spec.rb
+++ spec/unit/virtus/extensions/allowed_writer_methods_spec.rb
@@ -1,18 +1,6 @@
 require 'spec_helper'
 
-describe Virtus::Extensions, '#allowed_writer_methods' do
-  subject { object.allowed_writer_methods }
-
-  let(:object) do
-    Class.new do
-      include Virtus
-      attribute :virtus_attribute, String
-      attr_accessor :some_other_attribute
-    private
-      attr_accessor :private_attribute
-    end
-  end
-
+shared_examples_for "#allowed_writer_methods" do
   it { should include('virtus_attribute=') }
   it { should include('some_other_attribute=') }
   it { should_not include('private_attribute=') }
@@ -22,4 +10,44 @@ describe Virtus::Extensions, '#allowed_writer_methods' do
   end
 
   it { should be_frozen }
+end
+
+describe Virtus::Extensions, '#allowed_writer_methods' do
+  subject { object.send :allowed_writer_methods }
+
+  let(:klass) do
+    Class.new do
+      include Virtus
+      attribute :virtus_attribute, String
+      attr_accessor :some_other_attribute
+    private
+      attr_accessor :private_attribute
+    end
+  end
+
+  let(:mod) do
+    Module.new do
+      include Virtus
+      attribute :virtus_attribute, String
+      attr_accessor :some_other_attribute
+    private
+      attr_accessor :private_attribute
+    end
+  end
+
+  context "with a class" do
+    let(:object) { klass }
+
+    it_behaves_like "#allowed_writer_methods"
+  end
+
+  context "with an instance" do
+    let(:object) {
+      instance = Class.new { attr_accessor :some_other_attribute }.new.extend(Virtus)
+      instance.attribute :virtus_attribute, String
+      instance
+    }
+
+    it_behaves_like "#allowed_writer_methods"
+  end
 end
