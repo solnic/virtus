44249c73e9876af17d899a24be4c607f106d4ff1
Don Morrison <elskwid@gmail.com>
2013-06-04 16:07:30 -0700

    Remove Configuration#initialize spec
    Initialization is covered in call_spec

--- spec/unit/virtus/configuration/call_spec.rb
+++ spec/unit/virtus/configuration/call_spec.rb
@@ -1,8 +1,19 @@
 require 'spec_helper'
 
 describe Virtus::Configuration, '#call' do
-  subject { described_class.new.call { |config| config.coerce = false } }
+  subject { described_class.new.call(&block) } # { |config| config.coerce = false } }
 
-  its(:coerce)  { should eq(false) }
+  let(:block) { Proc.new {} }
+
   its(:coercer) { should be_instance_of(Coercible::Coercer) }
+
+  context "when coerce option isn't explicity set" do
+    its(:coerce) { should be(true) }
+  end
+
+  context "when coerce option is set to false" do
+    let(:block) { Proc.new { |config| config.coerce = false } }
+
+    its(:coerce) { should be(false) }
+  end
 end
--- spec/unit/virtus/configuration/initialize_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Virtus::Configuration, '#initialize' do
-  subject { described_class.new() }
-
-  its(:coerce) { should eq(true) }
-end
