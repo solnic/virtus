e44849f17a03ddf1dd30f2a127cdf5a10b5b57e4
Don Morrison <elskwid@gmail.com>
2013-06-04 16:13:43 -0700

    Document ModuleBuilder

--- lib/virtus/module_builder.rb
+++ lib/virtus/module_builder.rb
@@ -1,7 +1,24 @@
 module Virtus
 
+  # Class to build a Virtus module with it's own configuration
+  #
+  # This allows for individual Virtus modules to be included in
+  # classes and not impacted by the global Virtus configuration,
+  # which is implemented using Virtus::Configuration.
   class ModuleBuilder
 
+    # Builds a new Virtus module
+    #
+    # The block is passed to Virtus::Configuration
+    #
+    # @example
+    #   ModuleBuilder.call do |config|
+    #     # config settings
+    #   end
+    #
+    # @return [Module]
+    #
+    # @api public
     def self.call(&block)
       config  = Configuration.new.call(&block)
       builder = self.new(config)
@@ -9,6 +26,11 @@ module Virtus
       builder.mod
     end
 
+    # Initializes a new ModuleBuilder
+    #
+    # @return [undefined]
+    #
+    # @api private
     def initialize(configuration)
       @configuration = configuration
 
@@ -18,12 +40,20 @@ module Virtus
     end
 
     # Accessor for the anonymous module
+    #
+    # @return [Module]
+    #
+    # @api private
     def mod
       @mod
     end
 
     # Adds the .included hook to the anonymous module which then defines the
     # .attribute method to override the default.
+    #
+    # @return [Module]
+    #
+    # @api private
     def add_included_hook
       configuration    = @configuration
       attribute_method = self.attribute_method(configuration)
@@ -40,6 +70,10 @@ module Virtus
     # The coercer is passed in the unused key :configured_coercer to allow the
     # property encapsulation by Virtus::Attribute::Coercer, where the
     # coercion method is known.
+    #
+    # @return [Proc]
+    #
+    # @api private
     def attribute_method(configuration)
       Proc.new do |name, type, options = {}|
         module_options = {
@@ -50,6 +84,6 @@ module Virtus
         super(name, type, module_options.merge(options))
       end
     end
-  end
 
+  end # class ModuleBuilder
 end # module Virtus
