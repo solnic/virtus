8d6ea45d665bf428c812cfa7bc7a301e05ed2e87
Piotr Solnica <piotr.solnica@gmail.com>
2013-09-16 13:32:00 +0200

    Extend Virtus.attribute specs with more examples

--- spec/unit/virtus/attribute_spec.rb
+++ spec/unit/virtus/attribute_spec.rb
@@ -11,6 +11,16 @@ describe Virtus, '#attribute' do
     its(:instance_methods) { should include(:test?) }
   end
 
+  share_examples_for 'an object with string attribute' do
+    it { should respond_to(:test) }
+    it { should respond_to(:test=) }
+
+    it 'can write and read the attribute' do
+      subject.test = :foo
+      expect(subject.test).to eql('foo')
+    end
+  end
+
   context 'with a class' do
     context 'when type is Boolean' do
       before :all do
@@ -65,11 +75,50 @@ describe Virtus, '#attribute' do
     let(:mod) { Module.new { include Virtus } }
     let(:model) { Class.new }
 
-    it 'adds attributes from the module to a class that includes it' do
+    before do
       mod.attribute(:test, String)
       model.send(:include, mod)
+    end
 
+    it 'adds attributes from the module to a class that includes it' do
       expect(model.attribute_set[:test]).to be_instance_of(Virtus::Attribute)
+    end
+
+    it_behaves_like 'an object with string attribute' do
+      subject { model.new }
+    end
+  end
+
+  context 'with an instance' do
+    subject { model.new }
+
+    let(:model) { Class.new }
+
+    before do
+      subject.extend(Virtus)
+      subject.attribute(:test, String)
+    end
+
+    it_behaves_like 'an object with string attribute'
+  end
+
+  context 'using custom module' do
+    subject { model.new }
+
+    let(:model) {
+      Class.new {
+        include Virtus.module { |config| config.coerce = false }
+
+        attribute :test, String
+      }
+    }
+
+    it { should respond_to(:test) }
+    it { should respond_to(:test=) }
+
+    it 'writes and reads attributes' do
+      subject.test = :foo
+      expect(subject.test).to be(:foo)
     end
   end
 end
