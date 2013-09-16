6f18ab6462bcb7ceed9f2c36769caba60c6d44e9
Piotr Solnica <piotr.solnica@gmail.com>
2013-09-16 12:58:44 +0200

    Add specs for Attribute#set_default_value

---
+++ spec/unit/virtus/attribute/set_default_value_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+describe Virtus::Attribute, '#set_default_value' do
+  let(:object) { described_class.build(String, options.merge(:name => name, :default => default)) }
+
+  let(:model)    { Class.new { def name; 'model'; end; attr_reader :test } }
+  let(:name)     { :test }
+  let(:instance) { model.new }
+  let(:options)  { {} }
+
+  before { object.set_default_value(instance) }
+
+  context 'with a nil' do
+    subject { instance }
+
+    let(:default) { nil }
+
+    its(:test) { should be(nil) }
+    its(:instance_variables) { should include(:'@test') }
+  end
+
+  context 'with a clonable' do
+    subject { instance }
+
+    let(:default) { [] }
+
+    its(:test) { should eq(default) }
+    its(:test) { should_not be(default) }
+  end
+
+  context 'with a callable' do
+    subject { instance }
+
+    let(:default) { proc { |model, attribute| "#{model.name}-#{attribute.name}" } }
+
+    its(:test) { should eq('model-test') }
+  end
+
+  context 'with a symbol' do
+    subject { instance }
+
+    context 'when it is a method name' do
+      let(:default) { :set_test }
+      let(:model)   { Class.new { attr_reader :test; def set_test; @test = 'hello world'; end } }
+
+      its(:test) { should eq('hello world') }
+    end
+
+    context 'when it is not a method name' do
+      let(:default) { :hello_world }
+
+      its(:test) { should eq('hello_world') }
+    end
+  end
+end
