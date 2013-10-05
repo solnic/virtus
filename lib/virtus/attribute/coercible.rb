969dcc0f1c0649850cbedb0f12f752e78efcf19f
Piotr Solnica <piotr.solnica@gmail.com>
2013-10-05 11:16:05 +0200

    Add docs for Attribute::Coercible

--- lib/virtus/attribute/coercible.rb
+++ lib/virtus/attribute/coercible.rb
@@ -1,8 +1,14 @@
 module Virtus
   class Attribute
 
+    # Attribute extension providing coercion when setting an attribute value
+    #
     module Coercible
 
+      # Coerce value before setting
+      #
+      # @see Accessor#set
+      #
       # @api public
       def set(instance, value)
         super(instance, coerce(value))
