b50d5fc4309f38ef6cf9c4390b4c75282feb5f73
Piotr Solnica <piotr.solnica@gmail.com>
2013-10-05 11:33:03 +0200

    Add docs for Strict extension

--- lib/virtus/attribute/strict.rb
+++ lib/virtus/attribute/strict.rb
@@ -1,8 +1,14 @@
 module Virtus
   class Attribute
 
+    # Attribute extension which raises CoercionError when coercion failed
+    #
     module Strict
 
+      # @see [Attribute#coerce]
+      #
+      # @raises [CoercionError] when coercer failed
+      #
       # @api public
       def coerce(*)
         output = super
